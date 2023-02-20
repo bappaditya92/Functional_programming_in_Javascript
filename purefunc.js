b27115b725586981729c0d9abd7a65bad287e28b
{my <{bappadityasinha.92@gmail.com}>
2023-02-21 01:25:37 +0530

    first commit

---
+++ purefunc.js
@@ -0,0 +1,19 @@
+/*let cnt = 0;
+
+let increment = function(){
+    cnt++;
+    return cnt;
+};*/
+
+var x =1;
+func1();
+
+console.log(x);
+
+func2();
+
+console.log(x);
+
+func3();
+
+console.log(x);
